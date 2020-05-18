b6babfb5a4461ed2d7022d88443f12f0b026dc90
ceyhuncelik <ceyhuncelik.kbu@gmail.com>
2020-05-19 01:43:28 +0300

    first example
    ilk ornek yapıldı.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./styles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Button } from "react-bootstrap";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <Container fluid>
+      <div className="App">
+        <Button className="btn btn-success btn-sm"> asdasd </Button>
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+      </div>
+    </Container>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
+import LifeCycleExamples from "./LifeCycleExamples";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <LifeCycleExamples />
   </React.StrictMode>,
   rootElement
 );
